570fc890692b616b35db95277dda4ffffde7f21a
hacdan <david.mcclellan.p@gmail.com>
2024-06-24 12:01:14 -0400

    Go FMT

--- main.go
+++ main.go
@@ -1,104 +1,102 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "os"
+	"fmt"
+	"os"
+	"strings"
 
-    "github.com/juliangruber/go-intersect/v2"
+	"github.com/juliangruber/go-intersect/v2"
 )
 
 func main() {
-    if len(os.Args) == 1 {
-        // TODO: Write help message here
-        fmt.Println("No command supplied")
-        os.Exit(1)
-    }
+	if len(os.Args) == 1 {
+		// TODO: Write help message here
+		fmt.Println("No command supplied")
+		os.Exit(1)
+	}
 
-    command := os.Args[1]
-     
-    delimiter := findDelimter(os.Args[2])
-     
+	command := os.Args[1]
 
-    s1 := splitString(os.Args[2], delimiter)
-    s2 := splitString(os.Args[3], delimiter)
+	delimiter := findDelimter(os.Args[2])
 
-    switch command {
-    case "-u":
-        fmt.Println(findUnion(s1, s2))
-    case "-x":
-        fmt.Println(findIntersect(s1, s2))
-    case "-n":
-        fmt.Println(findComplements(s1, s2))
-    default:
-        // TODO: Print help message here along with error
-        fmt.Println("Incorrect command entered")
-    }
+	s1 := splitString(os.Args[2], delimiter)
+	s2 := splitString(os.Args[3], delimiter)
+
+	switch command {
+	case "-u", "--union":
+		fmt.Println(findUnion(s1, s2))
+	case "-x", "--intersect":
+		fmt.Println(findIntersect(s1, s2))
+	case "-n", "--complements":
+		fmt.Println(findComplements(s1, s2))
+	default:
+		// TODO: Print help message here along with error
+		fmt.Println("Incorrect command entered")
+	}
 }
 
 func removeDuplicateStr(strSlice []string) []string {
-    allKeys := make(map[string]bool)
-    list := []string{}
-    for _, item := range strSlice {
-        if _, value := allKeys[item]; !value {
-            allKeys[item] = true
-            list = append(list, item)
-        }
-    }
-    return list
+	allKeys := make(map[string]bool)
+	list := []string{}
+	for _, item := range strSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
 }
 
 func splitString(s string, d string) []string {
-    return cleanSlice(strings.Split(s, d))    
+	return cleanSlice(strings.Split(s, d))
 }
 
 func cleanSlice(s []string) []string {
-    c := []string{}
-    for _, item := range s {
-        c = append(c, strings.TrimSpace(item))
-    }
-    return c
+	c := []string{}
+	for _, item := range s {
+		c = append(c, strings.TrimSpace(item))
+	}
+	return c
 }
 
 func findComplements(s1, s2 []string) []string {
-    intersection := intersect.SimpleGeneric(s1, s2)
-    s3 := append(s1, s2...)
-    final := []string {}
+	intersection := intersect.SimpleGeneric(s1, s2)
+	s3 := append(s1, s2...)
+	result := []string{}
 
-    for _, s := range s3 {
-        if contains(s, intersection) != true {
-            final = append(final, s)      
-        }
-    }
-    return final
+	for _, s := range s3 {
+		if contains(s, intersection) != true {
+			result = append(result, s)
+		}
+	}
+	return result
 }
 
 func contains(s string, c []string) bool {
-    for _, str := range c {
-        if str == s {
-            return true
-        }
-    }
-    return false
+	for _, str := range c {
+		if str == s {
+			return true
+		}
+	}
+	return false
 }
 func findUnion(s1, s2 []string) []string {
-    s3 := append(s1, s2...)
-    return removeDuplicateStr(s3)
+	s3 := append(s1, s2...)
+	return removeDuplicateStr(s3)
 }
 
 func findIntersect(s1, s2 []string) []string {
-    return intersect.SimpleGeneric(s1, s2)
+	return intersect.SimpleGeneric(s1, s2)
 }
 
 func findDelimter(s string) string {
-    // Common delimiters - add to in the future
-    DELIMITERS := []string{",", " ", "\t", ";", "|", "\n"}
-    for _, c := range DELIMITERS {
-        if strings.Contains(s, c) {
-            return c
-        }
-    }
-    // TODO: Find way to return error here
-    return ""
+	// Common delimiters - add to in the future
+	DELIMITERS := []string{",", " ", "\t", ";", "|", "\n"}
+	for _, c := range DELIMITERS {
+		if strings.Contains(s, c) {
+			return c
+		}
+	}
+	// TODO: Find way to return error here
+	return ""
 }
-
